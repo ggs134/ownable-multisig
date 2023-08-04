f4914fa8febd0b0db5220c03b14361c3ceb658eb
zena <zena@tokamak.network>
2023-08-04 13:40:53 +0900

    Configure environment for verify.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,5 +7,40 @@ module.exports = {
   networks: {
     hardhat: {
     },
+    titan: {
+      url: `${process.env.ETH_NODE_URI_TITAN}`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      chainId: 55004
+    },
+    titangoerli: {
+      url: `${process.env.ETH_NODE_URI_TITAN_GOERLI}`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
+      chainId: 5050
+    }
   },
+  etherscan: {
+    apiKey: {
+      titan: "abc",
+      titangoerli: "abc"
+    } ,
+    customChains: [
+      {
+        network: "titan",
+        chainId: 55004,
+        urls: {
+          apiURL: "https://explorer.titan.tokamak.network/api",
+          browserURL: "https://explorer.titan.tokamak.network"
+        }
+      },
+      {
+        network: "titangoerli",
+        chainId: 5050,
+        urls: {
+          //https://tokamak-network.slack.com/archives/C01L9JLECLW/p1691027406871039
+          apiURL: "https://goerli.explorer.tokamak.network/api",
+          browserURL: "https://goerli.explorer.tokamak.network"
+        }
+      }
+    ]
+  }
 };
